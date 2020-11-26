c5477bd0500581bd603e357d5eb84fd492343d17
Oleg Chudilin <73231661+EraserH@users.noreply.github.com>
2020-11-26 14:36:36 +0400

    ничего не изменено, просто смотрю функции пайчарма

---
+++ main.py
@@ -0,0 +1,38 @@
+import typing
+
+
+def specialsort(inputelem):
+    return inputelem[1]
+
+
+import re
+
+fr: typing.TextIO = open('D:/testtext.txt', 'r')
+fw: typing.TextIO = open('D:/output.txt', 'w')
+words = []
+
+for line in fr:
+    words.extend(re.findall(r'\b\w+\b', line))
+print(words)
+
+words1: list[list[typing.Union[int, typing.Any]]] = []
+
+i = 0
+while words:
+    abc = words[0]
+    words1.append([abc, 0])
+    for word in words:
+        if word == abc:
+            (words1[i])[1] += 1
+            words.remove(word)
+    print(words)
+    i += 1
+words1.sort(key=specialsort, reverse=True)
+print(words1)
+quantity = int(input('\nВведите количество: '))
+for i in range(quantity):
+    fw.write(words1[i][0]+"\t"+str(words1[i][1])+"\n")
+fr.close()
+fw.close()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
